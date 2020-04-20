2c03eee6bf1bc4f57343e8db0aafc2b77269e107
wouerner <wouerner@protonmail.com>
2020-04-20 15:09:27 -0300

    Sync

--- html-page.tpl.php
+++ html-page.tpl.php
@@ -1,23 +1,69 @@
 <!--div id="cubo"></div-->
-<div id="widget"></div>
+<!--div id="widget"></div-->
+
+<div id="rnp-top-nav">
+  <div id="root"></div>
+</div>
+
+<script>
+/* function inicializar() { */
+/*   widget = NasNuvensWidget.new( */
+/*     { */
+/*       container : '#widget' , */
+/*       cubo: { */
+/*       corFundo: 'transparent',  //padrão: transparent */
+/*         corBlocos: '#000'  //padrão: #000 */
+/*       } */
+/*     } */
+/*   ); */
+
+/*   widget.renderCuboServicos() ; */
+/* } */
+
+/* inicializar(); */
+</script>
 
 <script>
 function inicializar() {
   widget = NasNuvensWidget.new(
     {
-      container : '#widget' ,
-      cubo: {
-      corFundo: 'transparent',  //padrão: transparent
-        corBlocos: '#000'  //padrão: #000
+      //seletor CSS do container a ser utilizado para renderizar o widget
+      container : '#root' ,
+      //site de origem (que está utilizando o widget)
+      origem: {
+        //nome utilizado em alguns pontos do widget e integração com outros serviços
+        nome: "Conferência Web",
+        //URL do logo tipo do site de origem
+        logo: "https://nasnuvens.gsc-hmg.rnp.br/img-servicos-temp/confweb.png",
+        //URL do site de origem, utilizado ao clicar na logo e em alguns fluxos da navbar
+        site: "https://conferenciaweb.rnp.br/",
+        //URL a ser utilizada pelo serviço de autenticação para enviar o redirecionamento pós-login,
+        //se não for informado será utilizado o atributo "site" da configuração.
+        //Exemplo com placeholder do EntityID recuperado da sessão CAFe:
+        //https://conferenciaweb.rnp.br/?idp=_REDIRECT_ENTITYID_
+        loginRedirect: null,
+        //URL de redirect após o logout do serviço de autenticação,
+        //se não for informado será utilizado o atributo "site" da configuração.
+        logoutRedirect: null
       }
-    }
-  );
+    });
 
-  widget.renderCuboServicos() ;
+  widget.render();
 }
 
 inicializar();
+
 </script>
+<style>
+#root {
+}
+
+#rnp-top-nav{
+  width: 1000px !important;
+  margin: 0 auto;
+  margin-top: 10px;
+}
+</style>
 
 <?php
 drupal_add_js('https://nasnuvens.gsc-hmg.rnp.br/navbar/widget.js', array(
